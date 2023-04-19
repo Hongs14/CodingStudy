323eeca2ce01a65b19e185ef091a7151ef880d24
hong <hongs09147@gmail.com>
2023-04-19 23:57:26 +0900

    2023.04.19 예산

--- src/programmers/level1/budget.java
+++ src/programmers/level1/budget.java
@@ -1,9 +1,25 @@
 package programmers.level1;
 
+import java.util.Arrays;
+
 public class budget {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] d = {1,3,2,5,4};
+		int budget = 9;
+		int answer = 0;
+		int sum=0;
+		
+		Arrays.sort(d);
+		
+		for(int j=0; j<d.length;j++) {
+			sum = sum + d[j];
+	        if(budget - sum >= 0) {
+	           answer += 1;
+	        }
+		}
+		
+		System.out.println(answer);
 
 	}
 
